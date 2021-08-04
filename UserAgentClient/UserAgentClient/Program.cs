b47ef9f0222cb752fff9c38f3ed0785175fde4c4
rgrishin <roman1988grishin@gmail.com>
2021-08-04 14:33:19 +0300

    Code sample

--- UserAgentClient/UserAgentClient/Program.cs
+++ UserAgentClient/UserAgentClient/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using SIPSorcery.Media;
+using SIPSorcery.SIP.App;
+using SIPSorceryMedia.Windows;
+
 
 namespace UserAgentClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string DESTINATION = "helloworld@sipsorcery.cloud";
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("SIP Get Started");
+
+            var userAgent = new SIPUserAgent();
+            var winAudio = new WindowsAudioEndPoint(new AudioEncoder());
+            var voipMediaSession = new VoIPMediaSession(winAudio.ToMediaEndPoints());
+
+            // Place the call and wait for the result.
+            bool callResult = await userAgent.Call(DESTINATION, null, null, voipMediaSession);
+            Console.WriteLine($"Call result {((callResult) ? "success" : "failure")}.");
+
+            Console.WriteLine("Press any key to hangup and exit.");
+            Console.ReadLine();
         }
     }
 }
